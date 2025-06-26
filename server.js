a2b373d83040eecd06343af0e0778843b47691af
OneShotted <hordes.iouser123@gmail.com>
2025-06-26 14:19:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,42 +1,47 @@
-const { WebSocketServer } = require("ws");
-const wss = new WebSocketServer({ port: 8080 });
+const WebSocket = require("ws");
+const wss = new WebSocket.Server({ port: 8080 });
 
 let players = {};
 let id = 0;
 
 wss.on("connection", (ws) => {
-  const playerId = id++;
-  players[playerId] = { x: Math.random() * 4000, y: Math.random() * 4000, name: "Unknown" };
+  const pid = id++;
+  players[pid] = {
+    x: Math.random() * 4000,
+    y: Math.random() * 4000,
+    name: "Unknown"
+  };
 
-  ws.send(JSON.stringify({ type: "init", id: playerId }));
+  ws.send(JSON.stringify({ type: "init", id: pid }));
 
   ws.on("message", (msg) => {
     let data;
     try {
       data = JSON.parse(msg);
-    } catch (e) {
+    } catch {
       return;
     }
 
     if (data.type === "join") {
-      players[playerId].name = data.name;
+      players[pid].name = data.name;
     } else if (data.type === "move") {
-      players[playerId].x = data.x;
-      players[playerId].y = data.y;
+      players[pid].x = data.x;
+      players[pid].y = data.y;
     }
   });
 
   ws.on("close", () => {
-    delete players[playerId];
+    delete players[pid];
   });
 });
 
 setInterval(() => {
   const state = JSON.stringify({ type: "state", players });
-  wss.clients.forEach((client) => {
-    if (client.readyState === 1) {
+  for (const client of wss.clients) {
+    if (client.readyState === WebSocket.OPEN) {
       client.send(state);
     }
-  });
-}, 1000 / 20);
+  }
+}, 50);
+
 
